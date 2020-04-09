d1393651f6d6fa68ef46aa259435fc3de66e3908
Paulo Victor Rios <victorios.ba@gmail.com>
2020-04-09 11:16:39 -0300

    configuration knex production

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,20 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const connection = knex(configuration.development);
+let connection = knex(configuration.development);
+
+
+let dbConnectionConfig;
+
+switch (process.env.NODE_ENV){
+  case 'production':
+    connection = knex(configuration.production);
+    break;
+  default:
+    connection = knex(configuration.development);
+}
+
+//const appDb = connectToDb(connection)
+//Model.knex(appDb)
 
 module.exports = connection;
